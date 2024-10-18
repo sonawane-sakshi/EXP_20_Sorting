d4303778cc980b24c68f5971cadc94a88ceb21b1
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-18 19:15:34 +0530

    Add files via upload

---
+++ bubble sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int arr[5] = {23, 12, 56, 144, 78};
+    int n = 5;
+
+    // Bubble Sort
+    for (int i = 0; i< n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] >arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+cout<< "The sorted array will be: ";
+    for (int i = 0; i< n; i++) {
+cout<<arr[i] << " ";
+    }
+cout<<endl; 
+    return 0;
+}
