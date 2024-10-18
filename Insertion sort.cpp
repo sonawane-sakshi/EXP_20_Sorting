3d681759633a70c7341851ef0cc6710e4041a256
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-18 19:18:04 +0530

    Add files via upload

---
+++ Insertion sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {45, 23, 86, 12, 9};
+    int n = 5;
+    // Insertion Sort
+    for (int i = 1; i < n; i++) {
+        int current = arr[i]; // The current element to be inserted
+        int j = i - 1;
+        // Shift elements of arr[0..i-1] that are greater than current to one position ahead of their current position
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current; // Insert the current element at its correct position
+    }
+    cout << "The sorted array will be: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
